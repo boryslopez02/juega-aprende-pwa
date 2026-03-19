0770c3104ff816f4ad8683aa3bcf9ce140f98157
Borys Lopez <borislopez1976@hotmail.com>
2026-03-19 09:33:37 -0400

    fix pwa

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
 import './assets/main.css'
 import './registerServiceWorker'
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm('Nueva versión disponible. ¿Recargar?')) {
+            updateSW(true)
+        }
+    },
+    onOfflineReady() {
+        console.log('Juega y Aprende está lista para funcionar sin internet')
+    }
+})
 
 const app = createApp(App)
 app.use(createPinia())
